fee70f128bdcf4975af712da75f13dcf82fcae7e
scagladurmaz <scagladurmaz@gmail.com>
2024-02-23 11:54:42 +0300

    Third-Commit

---
+++ Inheritance/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] persons = new Person[3]
+            {
+                new Customer{FirstName="Çağla"}, new Student{FirstName="Özgü"}, new Person{FirstName="Damla"} 
+            };
+
+            foreach (var person in persons)
+            {
+                Console.WriteLine(person.FirstName);
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+
+
+    //Bir nesneyi sadece bir kere inheritance yapabilirsiniz ama birden fazla implenetasyon(interface olacak yani) gerçekleştirebilirsiniz.
+    //Inheritance(classlardan) önce yazılır sonrasında kaç tane interface varsa yazabiliriz.
+    //classların tek başına bir anlamı var ancak interface'in tek başına bir anlamı yoktur.
+
+    //Senaryoya göre class veya interface implementasyonunu seçeceğiz.
+    //Amma zorunlu bir class(inheritance) dahilasyonu olduğunu düşünmüyorsak interface'lere yönelebiliriz
+
+    class Customer:Person 
+    {
+        public string City { get; set; }
+    }
+
+    class Student : Person
+    {
+        public string Department { get; set; }
+    }
+
+}
--- InterfacesDemo/Program.cs
+++ InterfacesDemo/Program.cs
@@ -2,16 +2,90 @@
 {
     class Program
     {
-        static  void Main(string[] args)
+        static void Main(string[] args)
         {
+            IWorker[] workers = new IWorker[3]
+            {
+                new Manager(),
+                new Worker(),
+                new Robot()
 
+            };
+
+            foreach (var worker in workers)
+            {
+                //Tüm personele çalış emrini vermiş oluyoruz
+                worker.Work();
+            }
+
+            IEat[] eats = new IEat[2]
+            {
+                new Worker(),
+                new Manager()
+            };
         }
     }
 
     interface IWorker
     {
         void Work();
+
+    }
+
+    //SOLID,Interface Segregation uyguladık
+    interface IEat
+    {
         void Eat();
+    }
+
+    interface ISalary
+    {
         void GetSalary();
+    }
+
+    //Bir class birden fazla interface implement edebilir.
+
+    class Manager : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Worker : IWorker, IEat, ISalary
+    {
+        public void Eat()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetSalary()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Robot : IWorker
+    {
+        public void Work()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
